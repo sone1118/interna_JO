fb285d3e5b6391f732e5e925fa45c46d3ce8dd90
sonhyejin <sone5414@gmail.com>
2022-11-29 18:04:57 +0900

    repository 부분 공부해야해.. 혜진아

---
+++ test11/jo/src/main/java/com/contentree/interna/user/repository/RememberUserRepository.java
@@ -0,0 +1,8 @@
+//package com.contentree.interna.user.repository;
+//
+//import com.contentree.interna.user.entity.User;
+//
+//public class RememberUserRepository implements UserRepository {
+//    public User findByUserEmail(String userEmail);
+//    public User findByUserSeq(Long userSeq)
+//}
--- test11/jo/src/main/java/com/contentree/interna/user/repository/UserRepository.java
+++ test11/jo/src/main/java/com/contentree/interna/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
-/*
- * package com.contentree.interna.user.repository;
- * 
- * import com.contentree.interna.user.entity.User; import
- * org.springframework.data.jpa.repository.JpaRepository;
- * 
- * public interface UserRepository extends JpaRepository<User, String>{
- * 
- * }
- */
+package com.contentree.interna.user.repository;
+
+import com.contentree.interna.user.entity.User; 
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface UserRepository extends JpaRepository<User, String>{
+
+	 User findBySeq(Long userSeq);
+	 User findByKakaoId(Long userKakaoId);
+	 
+ }
+ 
